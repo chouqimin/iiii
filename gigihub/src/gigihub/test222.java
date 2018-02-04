1eec4fa5f3d947de809a13d8a26a4605e3edfbec
jim7762000 <jim7762000@gmail.com>
2018-02-04 15:31:01 +0800

    update 15:30

--- gigihub/src/gigihub/test222.java
+++ gigihub/src/gigihub/test222.java
@@ -1,10 +1,10 @@
 package gigihub;
 
-public class TESTTEST {
+public class test222 {
 
 	public static void main(String[] args) {
-		System.out.println("YO1232");
-				
+		System.out.println("321");
+
 	}
 
 }
